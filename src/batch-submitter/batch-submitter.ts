fd58d3f909a4b940dcb870fb03107aa558e6df66
Karl Floersch <karl@karlfloersch.com>
2021-02-11 14:58:53 -0800

    Use 0 as min gas default not undefined

--- src/batch-submitter/batch-submitter.ts
+++ src/batch-submitter/batch-submitter.ts
@@ -181,7 +181,7 @@ export abstract class BatchSubmitter {
     this.lastBatchSubmissionTimestamp = Date.now()
     this.log.debug('Waiting for receipt...')
 
-    let minGasPrice = (this.minGasPriceInGwei)
+    let minGasPrice = (this.minGasPriceInGwei === 0)
       ? this.minGasPriceInGwei : ((await this.signer.getGasPrice()).div(1000000000)).toNumber() // convert to gwei
     if (minGasPrice < this.maxGasPriceInGwei) {
       this.log.warn('Minimum gas price is higher than max! Ethereum must be congested...')
